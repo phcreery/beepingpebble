b4ac3020d3cdcc58146eed79012c7e04087f6fa1
phcreery <phcreery@gmail.com>
2023-09-03 09:14:02 -0500

    fix rect size

--- src/draw.v
+++ src/draw.v
@@ -1,13 +1,17 @@
 module main
 
-// import gg
 import gx
 // import sokol.gfx
 import math
 import time
 import arrays
 
-import fbdev
+// $if rpi ? {
+// 	import fbdev as gg
+// } $else {
+	import gg
+// }
+
 
 // NOTE: in order to simulate the pixelated screen of 400x240, you need to
 // change line 472 of gg.c.v to `high_dpi: false`
@@ -28,43 +32,43 @@ mut:
 	frames	   int
 	fps 	   int
 	img_id       int
-	// gg_ctx       &gg.Context = unsafe { nil }
-	gg_ctx       &fbdev.Context = unsafe { nil }
+	gg_ctx       &gg.Context = unsafe { nil }
+	// gg_ctx       &fbdev.Context = unsafe { nil }
 }
 
-pub fn create_context(user_data voidptr, frame_fn fn (voidptr)) &Context { //, event_fn fn (&gg.Event, voidptr)
+pub fn create_context(user_data voidptr, frame_fn fn (voidptr), event_fn fn (&gg.Event, voidptr)) &Context { //, event_fn fn (&gg.Event, voidptr)
 	mut ctx := &Context{
 		pixel_buffer: []u8{len: line_length*height*components, cap: line_length*height*components, init:0}
-		// gg_ctx: &gg.Context{}
-		gg_ctx: &fbdev.Context{}
+		gg_ctx: &gg.Context{}
+		// gg_ctx: &fbdev.Context{}
 	}
 	// ---- GG ----
-	// ctx.gg_ctx = gg.new_context(
-	// 	bg_color: gx.white
-	// 	width: width
-	// 	height: height
-	// 	create_window: true
-	// 	window_title: 'BEEPINGPEBBLE'
-	// 	// init_fn: init_fn
-	// 	// init_fn: graphics_init
-	// 	init_fn: fn [mut ctx] (_ voidptr) {
-	// 		ctx.gg_ctx.new_streaming_image(width, height, 4, pixel_format: .rgba8)
-	// 	}
-	// 	frame_fn: frame_fn
-	// 	event_fn: event_fn
-	// 	user_data: user_data
-	// )
-
-	// ---- fbdev ----
-	ctx.gg_ctx = fbdev.new_context(
+	ctx.gg_ctx = gg.new_context(
 		bg_color: gx.white
 		width: width
 		height: height
-		
+		create_window: true
+		window_title: 'BEEPINGPEBBLE'
+		// init_fn: init_fn
+		// init_fn: graphics_init
+		init_fn: fn [mut ctx] (_ voidptr) {
+			ctx.gg_ctx.new_streaming_image(width, height, 4, pixel_format: .rgba8)
+		}
 		frame_fn: frame_fn
-		// event_fn: event_fn
+		event_fn: event_fn
 		user_data: user_data
 	)
+
+	// ---- fbdev ----
+	// ctx.gg_ctx = fbdev.new_context(
+	// 	bg_color: gx.white
+	// 	width: width
+	// 	height: height
+
+	// 	frame_fn: frame_fn
+	// 	// event_fn: event_fn
+	// 	user_data: user_data
+	// )
 
 
 
@@ -76,7 +80,6 @@ pub fn create_context(user_data voidptr, frame_fn fn (voidptr)) &Context { //, e
 pub fn (mut ctx Context) compute_fps() {
 	ctx.frames += 1
 	elapsed := time.since(ctx.fps_stopwatch)
-	// println("elapsed ${elapsed}")
 	if elapsed.nanoseconds() > 1_000_000_000 {
 		ctx.fps = int(ctx.frames)
 		println("fps ${ctx.fps}")
@@ -110,35 +113,34 @@ pub fn (mut ctx Context) clear() {
 pub fn (mut ctx Context) blit() {
 
 	// ---- gg ----
-	// // mut buffer := [][]u32{len: height, init: []u32{len: width}}
-	// mut buffer := [height][width]u32{}
-	// for y in 0 .. height {
-	// 	for x in 0 .. width {
-	// 		// convert from BGRA8 to RGBA8
-	// 		pos := u64(y*line_length+x*components)
-	// 		// println("pos ${pos}")
-	// 		blue := ctx.pixel_buffer[pos+0]
-	// 		green:= ctx.pixel_buffer[pos+1]
-	// 		red:= ctx.pixel_buffer[pos+2]
-	// 		// a: ctx.pixel_buffer[u64((line_length*y+x))+3]
-	// 		buffer[y][x] = u32((red | (u32(green) << 8) | (u32(blue) << 16) | (0xFF << 24)))
+	mut buffer := [height][width]u32{}
+	for y in 0 .. height {
+		for x in 0 .. width {
+			// convert from BGRA8 to RGBA8
+			pos := u64(y*line_length+x*components)
+			// println("pos ${pos}")
+			blue := ctx.pixel_buffer[pos+0]
+			green:= ctx.pixel_buffer[pos+1]
+			red:= ctx.pixel_buffer[pos+2]
+			// a: ctx.pixel_buffer[u64((line_length*y+x))+3]
+			buffer[y][x] = u32((red | (u32(green) << 8) | (u32(blue) << 16) | (0xFF << 24)))
 
-	// 	}
-	// }
-	// mut img := ctx.gg_ctx.get_cached_image_by_idx(ctx.img_id)
-	// img.update_pixel_data(unsafe { &u8(&buffer) })
-	// ctx.gg_ctx.draw_image(0, 0, width, height, img)
+		}
+	}
+	mut img := ctx.gg_ctx.get_cached_image_by_idx(ctx.img_id)
+	img.update_pixel_data(unsafe { &u8(&buffer) })
+	ctx.gg_ctx.draw_image(0, 0, width, height, img)
 
 	// ---- fbdev ----
-	ctx.gg_ctx.blit(ctx.pixel_buffer)
+	// ctx.gg_ctx.blit(ctx.pixel_buffer)
+	// OR
+	// ctx.gg_ctx.framebuffer.write_to(0, ctx.pixel_buffer) or {}
 }
 
 [inline]
 pub fn (mut ctx Context) draw_pixel(x_ f32, y_ f32, c gx.Color) {
 	x:=int(x_)
 	y:=int(y_)
-	// println("draw_pixel_inv x ${x} y ${y}")
-	// println("pixel_buffer len ${ctx.pixel_buffer.len}")
 
 	if x < 0 || x >= width || y < 0 || y >= height { return }
 	pos := u64(y*line_length+x*components)
@@ -219,37 +221,33 @@ pub fn (mut ctx Context) draw_line_inv(x f32, y f32, x1 f32, y1 f32) {
 }
 
 pub fn (mut ctx Context) draw_rect_filled(x f32, y f32, w f32, h f32, c gx.Color) {
-	// ctx.gg_ctx.draw_rect_filled(x, y, w, h, c)
-
 	ix := int(x)
 	iy := int(y)
 	iw := int(w)
 	ih := int(h)
 
-	for yy in iy .. iy + ih {
-		for xx in ix .. ix + iw {
+
+	for yy in iy .. iy + ih+1 {
+		for xx in ix .. ix + iw+1 {
 			ctx.draw_pixel(xx, yy, c)
 		}
 	}
 }
 
 pub fn (mut ctx Context) draw_rect_filled_inv(x f32, y f32, w f32, h f32) {
-	// ctx.gg_ctx.draw_rect_filled(x, y, w, h, c)
-
 	ix := int(x)
 	iy := int(y)
 	iw := int(w)
 	ih := int(h)
 
-	for yy in iy .. iy + ih {
-		for xx in ix .. ix + iw {
+	for yy in iy .. iy + ih+1 {
+		for xx in ix .. ix + iw+1 {
 			ctx.draw_pixel_inv(xx, yy)
 		}
 	}
 }
 
 pub fn (mut ctx Context) draw_rect_empty(x f32, y f32, w f32, h f32, c gx.Color) {
-	// ctx.gg_ctx.draw_rect_empty(x, y, w, h, c)
 	ctx.draw_line(x, y, x + w, y, c)
 	ctx.draw_line(x + w, y, x + w, y + h, c)
 	ctx.draw_line(x + w, y + h, x, y + h, c)
@@ -257,7 +255,6 @@ pub fn (mut ctx Context) draw_rect_empty(x f32, y f32, w f32, h f32, c gx.Color)
 }
 
 pub fn (mut ctx Context) draw_rect_empty_inv(x f32, y f32, w f32, h f32) {
-	// ctx.gg_ctx.draw_rect_empty(x, y, w, h, c)
 	ctx.draw_line_inv(x, y, x + w, y)
 	ctx.draw_line_inv(x + w, y, x + w, y + h)
 	ctx.draw_line_inv(x + w, y + h, x, y + h)
@@ -271,8 +268,6 @@ pub fn (mut ctx Context) draw_polygon(points []Point, c gx.Color) {
 }
 
 pub fn (mut ctx Context) draw_polygon_filled(points []Point, c gx.Color) {
-	// ctx.gg_ctx.draw_polygon_filled(points, c)
-
 	num_coreners := points.len
 	mut vx := []f32{}
 	mut vy := []f32{}
@@ -294,7 +289,6 @@ pub fn (mut ctx Context) draw_polygon_filled(points []Point, c gx.Color) {
 		for i in 0 .. num_coreners {
 			if (points[i].y < py && points[j].y >= py) || (points[j].y < py && points[i].y >= py) {
 				nodes += 1
-				// nodes_x << (points[i].x + (py - points[i].y) / (points[j].y - points[i].y) * (points[j].x - points[i].x))
 				nodes_x[nodes] = (points[i].x + (py - points[i].y) / (points[j].y - points[i].y) * (points[j].x - points[i].x))
 			}
 			j = i
