53d3c5d1e78d87d5f3de693b92929bb71972034c
phcreery <phcreery@gmail.com>
2023-09-02 21:45:11 -0500

    more keyboard shortcuts

--- src/main.v
+++ src/main.v
@@ -17,7 +17,8 @@ fn draw(mut app App) {
 	app.menu.draw(mut app.ctx)
 	app.ctx.draw_line_inv(0, 0, 20, 60)
 	app.ctx.draw_line(20, 60, 20, 160, gx.red)
-	app.ctx.draw_polygon_filled([Point{10, 10}, Point{20, 20}, Point{30, 10}], gx.green)
+	app.ctx.draw_polygon_filled([Point{10, 10}, Point{20, 20},
+		Point{30, 10}], gx.green)
 	app.ctx.draw_polygon([Point{10, 100}, Point{20, 200}, Point{30, 100}], gx.red)
 	// app.ctx.draw_line_inv(0, 10, 20, 30)
 	// app.ctx.draw_test_image()
@@ -32,17 +33,17 @@ fn event_manager(mut ev gg.Event, mut app App) {
 				// app.ctx.quit()
 				println('escape')
 			}
-			.right {
-				app.menu.next()
-			}
-			.left {
-				app.menu.prev()
-			}
-			.up {
+			.up, .w {
 				app.menu.up()
 			}
-			.down {
+			.left, .a {
+				app.menu.prev()
+			}
+			.down, .s {
 				app.menu.down()
+			}
+			.right, .d {
+				app.menu.next()
 			}
 			else {
 				println('key: ')
